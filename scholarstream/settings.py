8c1cc5fe33d25410346b01094f05e0e9bcccf60f
Akshay Kumar <akshay.kumar19002@gmail.com>
2023-06-18 09:54:19 -0400

    Changes db from sqlite to postgres

--- scholarstream/settings.py
+++ scholarstream/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-sq1o^(y^1fgrc)avfrvd-yp01&_3tffh6=*8%^6+r$eycy=8$7'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
