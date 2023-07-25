cfcf0192edbd8748cb10382b700568b5236d0594
Kiran Prasad Puthan <41895796+kiran867@users.noreply.github.com>
2023-07-24 20:45:17 -0400

    Added Name of Course, Updated certificate ID to unique string of 128 Character length, updated text position and colour

--- course/utils.py
+++ course/utils.py
@@ -1,45 +1,41 @@
 import os
 import cv2
-from datetime import datetime
+from PIL import Image, ImageDraw, ImageFont
 
-def generate_certificate_image(name, course_name, certificate_id):
+
+def generate_certificate_image(user, course, certificate_id, date):
     template_path = "certificate-template.jpg"
     output_folder = "generated-certificates/"
     os.makedirs(output_folder, exist_ok=True)
 
-    certificate_image = cv2.imread(template_path)
-    font = cv2.FONT_HERSHEY_SIMPLEX
-    font_scale = 1.2
-    font_thickness = 2
-    text_color = (0, 0, 0) 
+    certificate_image = Image.open(template_path)
+    draw = ImageDraw.Draw(certificate_image)
 
-    (name_width, name_height), _ = cv2.getTextSize(name, font, font_scale, font_thickness)
-    name_position_x = int((certificate_image.shape[1] - name_width) / 2)
-    name_position_y = int(certificate_image.shape[0] * 0.4)
+    font_path = "path_to_your_font_file.ttf"  # Replace with the path to the font file you want to use
+    font_size_name = 50
+    font_size_course = 40
+    font_size_id_date = 12
 
+    font_name = ImageFont.truetype(font_path, font_size_name)
+    font_course = ImageFont.truetype(font_path, font_size_course)
+    font_id_date = ImageFont.truetype(font_path, font_size_id_date)
 
-    cv2.putText(certificate_image, name, (name_position_x, name_position_y), font, font_scale, text_color, font_thickness,
-                cv2.LINE_AA)
+    img_width, img_height = certificate_image.size
 
+    position_x_name = int(img_width / 2)
+    position_x_course = int(img_width / 2)
+    position_x_id_date = int(img_width / 2)
+    position_y_name = 700
+    position_y_course = 800
+    position_y_id_date = img_height - 50
 
-    (course_width, course_height), _ = cv2.getTextSize(course_name, font, font_scale, font_thickness)
-    course_position_x = int((certificate_image.shape[1] - course_width) / 2)
-    course_position_y = int(certificate_image.shape[0] * 0.6)
+    draw.text((position_x_name, position_y_name), user.full_name, fill=(128, 0, 0), font=font_name, anchor="mm")
 
+    draw.text((position_x_course, position_y_course), course.name, fill=(0, 0, 0), font=font_course, anchor="mm")
 
-    cv2.putText(certificate_image, course_name, (course_position_x, course_position_y), font, font_scale, text_color, font_thickness,
-                cv2.LINE_AA)
+    draw.text((position_x_id_date, position_y_id_date), f"Certificate ID: {certificate_id}", fill=(0, 0, 0), font=font_id_date, anchor="mm")
+    draw.text((position_x_id_date, position_y_id_date + 20), f"Date: {date}", fill=(0, 0, 0), font=font_id_date, anchor="mm")
 
-
-    certificate_id_date = f"Certificate ID: {certificate_id} - {datetime.now().strftime('%Y-%m-%d')}"
-    (id_date_width, id_date_height), _ = cv2.getTextSize(certificate_id_date, font, 0.6, font_thickness)
-    id_date_position_x = int((certificate_image.shape[1] - id_date_width) / 2)
-    id_date_position_y = int(certificate_image.shape[0] * 0.9)
-
-
-    cv2.putText(certificate_image, certificate_id_date, (id_date_position_x, id_date_position_y), font, 0.6, text_color, font_thickness,
-                cv2.LINE_AA)
-
-    output_path = os.path.join(output_folder, f"{name}_certificate.jpg")
-    cv2.imwrite(output_path, certificate_image)
-    print(f"Certificate generated for {name} - Certificate ID: {certificate_id} - Date: {datetime.now().strftime('%Y-%m-%d')}")
+    output_path = os.path.join(output_folder, f"{user.full_name}_{course.name}_certificate.jpg")
+    certificate_image.save(output_path)
+    return output_path
